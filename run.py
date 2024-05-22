0d6f3869bcab1a479249984cf36a2681111abc82
Abigail Ryan <abi.ryan91@gmail.com>
2024-05-22 19:31:01 +0000

    Add request user input to start the game, only accepts valid input

--- run.py
+++ run.py
@@ -16,12 +16,26 @@ def print_logo():
     print(logo)
 print_logo()
 
-print("Game Rules:")
+print("Hangman Game Rules:")
 print("- The computer generates a random word and you have to guess it letter by letter.")
 print("- If you guess the correct letter, it will be placed on the game board.")
 print("- If you complete the word you win the game.")
 print("- If you guess the wrong letter, you'll lose a guess & add a piece to the Hangman.")
 print("- If you use up all your guesses and the Hangman is complete, you lose the game.\n")
 
-play = input("Do you want to play? Enter 'Y' for yes or 'N' for no: \n").upper()
+"""
+Validates user input. Requires user to enter only Y or N.
+If user inputs invalid choice the loop runs until one of the options is selected.
+"""
+while True:
+    choice = input("Do you want to play? Enter 'Y' for yes or 'N' for no: \n").upper()
+    if choice == "Y" :
+        play_game()
+    elif choice == "N" :
+        print("Sorry to see you go. Come back and play another time!")
+        break
+    else:
+        print("Invalid option. Please only input 'Y' or 'N'")
+       
+
 
